e9452fb3fdd616897b3e0cab671e9aa8daa98447
Walter <walter.voss@gmail.com>
2023-10-03 16:49:01 -0400

    1

--- utils/calculate_accuracy.py
+++ utils/calculate_accuracy.py
@@ -7,11 +7,11 @@ def calculate_accuracy(original_text, transcript):
         messages=[
             {
                 "role": "system",
-                "content": "You will rate the accuracy of the spoken transcript compared to the original text on a scale of 1 to 3. 1 indicates significant deviations from the original, 3 indicates almost perfect or perfect accuracy, and 2 falls in between. We're evaluating the student's ability to read accurately, focusing on phonics and word recognition. Overlook minor differences in capitalization, punctuation, or slight word variations. The essence of the message is more important than exact wording. If the transcript captures the general idea but has some errors or word substitutions, it should likely be a 2. If it's nearly perfect, it's a 3. If it's largely incorrect or misses the main point, it's a 1. Please respond with only a number: 1, 2, or 3.",
+                "content": "You will rate the accuracy of the spoken transcript compared to the original text on a scale of 1 to 3. A '1' indicates that the transcript is largely inaccurate or misses the main point of the original text. A '3' means the transcript is almost perfect or perfectly accurate. A '2' is given when the transcript captures the general idea of the original text, even if there are some errors, word substitutions, or slight changes in phrasing. The overall intent and meaning are more important than exact word-for-word accuracy. Please provide a rating based on this guidance: 1, 2, or 3.",
             },
             {
                 "role": "user",
-                "content": f"Original: {original_text}. Transcript: {transcript}. How accurate is the transcript?",
+                "content": f"Original: {original_text}. Transcript: {transcript}. Based on the guidance, how accurate is the transcript?",
             },
         ],
     )
