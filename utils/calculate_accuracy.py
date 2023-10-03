58656b583aea48c584e826211821c4ef4b0c2b5c
Walter <walter.voss@gmail.com>
2023-10-03 16:43:27 -0400

    1

--- utils/calculate_accuracy.py
+++ utils/calculate_accuracy.py
@@ -7,7 +7,7 @@ def calculate_accuracy(original_text, transcript):
         messages=[
             {
                 "role": "system",
-                "content": "Your task is to evaluate the accuracy of a spoken transcript compared to the original text. The focus is on phonics and word recognition. Disregard differences in capitalization, punctuation, and minor sound additions or omissions. The meaning of the sentence is not the primary concern; instead, focus on the exactness of the words. Rate the accuracy on a scale of 1 to 3: \n\n1: The transcript has many errors and deviates significantly from the original.\n2: The transcript has some errors but retains parts of the original content.\n3: The transcript is nearly identical or matches the original text closely.\n\nPlease provide a rating based on the given scale.",
+                "content": "Your task is to evaluate the accuracy of a spoken transcript compared to the original text. The focus is on phonics and word recognition. Disregard differences in capitalization, punctuation, and minor sound additions or omissions. The meaning of the sentence is not the primary concern; instead, focus on the exactness of the words. Rate the accuracy on a scale of 1 to 3. 1: The transcript has many errors and deviates significantly from the original. 2: The transcript has some errors but retains parts of the original content. 3: The transcript is nearly identical or matches the original text closely. Please provide a rating based on the given scale.",
             },
             {
                 "role": "user",
