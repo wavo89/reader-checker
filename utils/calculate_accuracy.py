412491c273b525a38584e730ccd0a0abcdbd1259
Walter <walter.voss@gmail.com>
2023-10-03 16:42:36 -0400

    1

--- utils/calculate_accuracy.py
+++ utils/calculate_accuracy.py
@@ -1,10 +1,13 @@
+import openai
+
+
 def calculate_accuracy(original_text, transcript):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "system",
-                "content": "Your task is to evaluate the accuracy of a spoken transcript compared to the original text. The focus is on phonics and word recognition. Disregard differences in capitalization, punctuation, and minor sound additions or omissions. The meaning of the sentence is not the primary concern; instead, focus on the exactness of the words. Rate the accuracy on a scale of 1 to 3. 1: The transcript has many errors and deviates significantly from the original. 2: The transcript has some errors but retains parts of the original content. 3: The transcript is nearly identical or matches the original text closely. Please provide a rating based on the given scale.",
+                "content": "Your task is to evaluate the accuracy of a spoken transcript compared to the original text. The focus is on phonics and word recognition. Disregard differences in capitalization, punctuation, and minor sound additions or omissions. The meaning of the sentence is not the primary concern; instead, focus on the exactness of the words. Rate the accuracy on a scale of 1 to 3: \n\n1: The transcript has many errors and deviates significantly from the original.\n2: The transcript has some errors but retains parts of the original content.\n3: The transcript is nearly identical or matches the original text closely.\n\nPlease provide a rating based on the given scale.",
             },
             {
                 "role": "user",
