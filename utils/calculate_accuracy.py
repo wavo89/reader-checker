ea834a1b66797d92663ce9a23ea29aec8d0b9267
Walter <walter.voss@gmail.com>
2023-10-03 15:47:14 -0400

    1

--- utils/calculate_accuracy.py
+++ utils/calculate_accuracy.py
@@ -7,7 +7,7 @@ def calculate_accuracy(original_text, transcript):
         messages=[
             {
                 "role": "system",
-                "content": "You will rate the accuracy of the transcript compared to the original text on a scale of 1 to 3. 1 being the least accurate and 3 being the most accurate.",
+                "content": "You will rate the accuracy of the transcript compared to the original text on a scale of 1 to 3. 1 being the least accurate and 3 being the most accurate. Please respond with only a number: 1, 2, or 3.",
             },
             {
                 "role": "user",
@@ -15,8 +15,11 @@ def calculate_accuracy(original_text, transcript):
             },
         ],
     )
-    response_content = completion.choices[0].message.content
-    # Extract the number from the response
-    accuracy = int("".join(filter(str.isdigit, response_content.split()[-1])))
 
-    return accuracy
+    response_content = completion.choices[0].message.content.strip()
+
+    # Validate the response
+    if response_content not in ["1", "2", "3"]:
+        raise ValueError("Unexpected response from the model.")
+
+    return int(response_content)
