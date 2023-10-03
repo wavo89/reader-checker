35c2eb3316449c6597bdb661c3eb954daefed488
Walter <walter.voss@gmail.com>
2023-10-03 17:44:40 -0400

    1

--- utils/calculate_accuracy.py
+++ utils/calculate_accuracy.py
@@ -15,34 +15,38 @@ def calculate_accuracy(original_text, transcript):
     original_words = original_text.split()
     transcript_words = transcript.split()
 
-    # Calculate word accuracy
-    matching_words = sum(1 for o, t in zip(original_words, transcript_words) if o == t)
-    total_words = len(original_words)
-    word_accuracy = matching_words / total_words
+    # Calculate word accuracy with adjusted weights
+    total_weight = sum(0.25 if len(o) <= 3 else 1 for o in original_words)
+    matching_weight = sum(
+        0.25 if len(o) <= 3 else 1
+        for o, t in zip(original_words, transcript_words)
+        if o == t
+    )
+    word_accuracy = matching_weight / total_weight
 
     # Calculate word order accuracy
     order_matches = 0
     for i in range(min(len(original_words), len(transcript_words))):
         if original_words[i] == transcript_words[i]:
             order_matches += 1
-    order_accuracy = order_matches / total_words
+    order_accuracy = order_matches / len(original_words)
 
     # Calculate overall accuracy percentage
     overall_accuracy = (word_accuracy + order_accuracy) / 2
 
     # Print the percentages and a summary of the numbers
     print(
-        f"Word Accuracy: {word_accuracy*100:.2f}% (Matching Words: {matching_words}/{total_words})"
+        f"Word Accuracy: {word_accuracy*100:.2f}% (Weighted Matching Words: {matching_weight:.2f}/{total_weight})"
     )
     print(
-        f"Order Accuracy: {order_accuracy*100:.2f}% (Correct Order Matches: {order_matches}/{total_words})"
+        f"Order Accuracy: {order_accuracy*100:.2f}% (Correct Order Matches: {order_matches}/{len(original_words)})"
     )
     print(f"Overall Accuracy: {overall_accuracy*100:.2f}%")
 
     # Return a score based on the accuracy percentage
     if 0 <= overall_accuracy <= 0.25:
         return 1
-    elif 0.26 <= overall_accuracy <= 0.84:  # Adjusted the range to avoid overlap
+    elif 0.26 <= overall_accuracy <= 0.84:
         return 2
     else:
         return 3
