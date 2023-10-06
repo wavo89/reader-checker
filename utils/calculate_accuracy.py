ed630ceaacd123bc607a98a627045270b75ab86a
Walter <walter.voss@gmail.com>
2023-10-05 20:14:43 -0400

    1

--- utils/calculate_accuracy.py
+++ utils/calculate_accuracy.py
@@ -27,9 +27,7 @@ def calculate_word_accuracy(original_words, transcript_words):
     for o in original_clone:
         if o in transcript_clone:
             matching_weight += 1
-            transcript_clone.remove(
-                o
-            )  # Remove the matched word so it won't be used again
+            transcript_clone.remove(o)
 
     # Check for partial matches among the unmatched words
     for o in [word for word in original_clone if word not in transcript_clone]:
@@ -38,15 +36,15 @@ def calculate_word_accuracy(original_words, transcript_words):
                 common_chars = sum(1 for char in o if char in t)
                 overlap_percent = common_chars / len(o)
                 if overlap_percent >= 0.7:  # Threshold for partial matches
-                    matching_weight += overlap_percent  # Add the overlap percent to the matching weight
-                    transcript_clone.remove(
-                        t
-                    )  # Remove the matched word so it won't be used again
+                    matching_weight += overlap_percent
+                    transcript_clone.remove(t)
                     break
 
-    # Further adjusting the penalty factor
+    # Further adjusting the penalty factor using a dynamic approach
     missing_words = total_words - matching_weight
-    penalty_factor = 1 - (0.02 * missing_words)
+    base_penalty = 0.05
+    dynamic_penalty = base_penalty * (10 / (total_words + 1))
+    penalty_factor = 1 - (dynamic_penalty * missing_words)
 
     return (matching_weight / total_words) * penalty_factor
 
@@ -71,27 +69,40 @@ def calculate_order_accuracy(original_words, transcript_words):
     return lcs / len(original_words)
 
 
-def calculate_accuracy(original_text, transcript):
-    original_text = preprocess_text(original_text)
-    transcript = preprocess_text(transcript)
-    original_words = original_text.split()
-    transcript_words = transcript.split()
-    word_accuracy = calculate_word_accuracy(original_words, transcript_words)
-    order_accuracy = calculate_order_accuracy(original_words, transcript_words)
-    overall_accuracy = round((0.8 * word_accuracy + 0.2 * order_accuracy), 2)
-    print(
-        f"Word Accuracy: {word_accuracy*100:.2f}% (Weighted Matching Words: {word_accuracy*len(original_words):.2f}/{len(original_words)})"
-    )
-    print(
-        f"Order Accuracy: {order_accuracy*100:.2f}% (Correct Order Matches: {order_accuracy*len(original_words):.2f}/{len(original_words)})"
-    )
-    print(f"Overall Accuracy: {overall_accuracy*100:.2f}%")
+def calculate_word_accuracy(original_words, transcript_words):
+    total_words = len(original_words)
+    matching_weight = 0
 
-    lower_threshold = 0.40
-    upper_threshold = 0.85
-    if 0 <= overall_accuracy <= lower_threshold:
-        return 1
-    elif lower_threshold <= overall_accuracy <= upper_threshold:
-        return 2
-    else:
-        return 3
+    # Clone the lists to not modify the original lists during removals
+    original_clone = original_words.copy()
+    transcript_clone = transcript_words.copy()
+
+    # Check for exact matches first
+    for o in original_clone:
+        if o in transcript_clone:
+            matching_weight += 1
+            transcript_clone.remove(o)
+
+    # Check for partial matches among the unmatched words
+    for o in [word for word in original_clone if word not in transcript_clone]:
+        if len(o) > 3:  # Only consider longer words for partial matches
+            for t in transcript_clone:
+                common_chars = sum(1 for char in o if char in t)
+                overlap_percent = common_chars / len(o)
+                if overlap_percent >= 0.7:  # Threshold for partial matches
+                    matching_weight += overlap_percent
+                    transcript_clone.remove(t)
+                    break
+
+    # Further adjusting the penalty factor using a more aggressive approach
+    missing_words = total_words - matching_weight
+    base_penalty = 0.1
+    dynamic_penalty = base_penalty * (10 / (total_words + 1))
+    penalty_factor = 1 - (base_penalty + dynamic_penalty) * missing_words
+
+    # Ensure word accuracy doesn't go below 0%
+    return max(0, (matching_weight / total_words) * penalty_factor)
+
+
+# Testing the updated function with the provided example
+calculate_word_accuracy_v7(original_words_example, transcript_words_example)
